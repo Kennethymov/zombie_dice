259b5c990b3332ef63be498cfe97af59304a17ec
William Cesar Santos <william_cesar_santos@hotmail.com>
2022-12-12 21:34:44 -0300

    Update

---
+++ src/br/com/ada/zombiedice/dado/DadoAmarelo.java
@@ -0,0 +1,16 @@
+package br.com.ada.zombiedice.dado;
+
+public class DadoAmarelo extends Dado {
+
+    public DadoAmarelo() {
+        super(new Face[]{
+                Face.CEREBRO,
+                Face.TIRO,
+                Face.PASSOS,
+                Face.CEREBRO,
+                Face.TIRO,
+                Face.PASSOS
+        });
+    }
+
+}
---
+++ src/br/com/ada/zombiedice/dado/DadoVermelho.java
@@ -0,0 +1,15 @@
+package br.com.ada.zombiedice.dado;
+
+public class DadoVermelho extends Dado {
+
+    public DadoVermelho() {
+        super(new Face[]{
+                Face.CEREBRO,
+                Face.TIRO,
+                Face.PASSOS,
+                Face.TIRO,
+                Face.PASSOS,
+                Face.TIRO
+        });
+    }
+}
